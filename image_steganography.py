841d088fe3ffc33e85202a72dddcd13a08dd0b07
Satya Vishal Thota <skillzworks.18@gmail.com>
2020-05-07 00:26:35 +0530

    Defined a function to convert the ASCII values to 8bit binary Value

--- image_steganography.py
+++ image_steganography.py
@@ -1,13 +1,31 @@
+#Takes integer as input and returns an 8bit binary value
+def decimal_bin_conversion(x):
+    l=[]
+    while (x!=0) :
+        l.append(str(x % 2))
+        x=int(x/2)
+    ln=len(l)
+    if(ln!=8):
+        for i in range(0,(8-ln)):
+            l.append(str(0))
+    l.reverse()
+    return "".join(l)
+
 # function converts the message to binary value list
 def msg_bin_conversion():
     msg=str(input("Enter your message here: \n"))
     #converts each character to the bin of its acsii value and joins it to the string.
     # The string is then split into a list
-    msg_bin=(" ".join(f"{ord(i):08b}" for i in msg)).split(sep=" ")
+    #msg_bin2=(" ".join(f"{ord(i):08b}" for i in msg)).split(sep=" ")
+    msg_bin=[]
+    for i in msg:
+        msg_bin.append(decimal_bin_conversion(ord(i)))
     print(msg_bin)
-    bin_msg_conversion(msg_bin)
-#msg_bin_conversion()
+    #print(msg_bin2)
+    #bin_msg_conversion(msg_bin)
+msg_bin_conversion()
 
+#Takes 8 bit binary as input and returns the decimal equivalent which is the ACSII value
 def bin_decimal_conversion(x):
     c=7
     decimal=0
@@ -16,11 +34,10 @@ def bin_decimal_conversion(x):
         c-=1
     return decimal
 
+#Takes the List of decimal equivalent of the characters of decoded Image and returns the message
 def bin_msg_conversion(bin_value):
     msg_list=[]
     for i in bin_value:
         msg_list.append(chr(bin_decimal_conversion(i)))
     msg="".join(msg_list)
     print("converted : ",msg)
-
-msg_bin_conversion()
