eb93dadff760e0bf76937713dc68dbb3c263ec67
Satya Vishal Thota <skillzworks.18@gmail.com>
2020-05-07 03:44:27 +0530

    Defined a new function initializer() which controls the flow of image encoding and decoding. Also added a clear() which clears the console screen.

--- image_steganography.py
+++ image_steganography.py
@@ -1,4 +1,16 @@
 from PIL import Image
+from os import system,name
+import time
+
+#Clears the console screen
+def clear():
+    #For windows
+    if name=='nt':
+        _=system('cls')
+    #For mac os and linux
+    else:
+        _=system('clear')
+
 #Takes integer as input and returns an 8bit binary value
 def decimal_bin_conversion(x):
     l=[]
@@ -13,15 +25,16 @@ def decimal_bin_conversion(x):
     return "".join(l)
 
 # function converts the message to binary value list
-def msg_bin_conversion():
-    msg=str(input("Enter your message here: \n"))
+def msg_bin_conversion(msg):
+    #msg=str(input("Enter your message here: \n"))
     #converts each character to the bin of its acsii value and joins it to the string.
     # The string is then split into a list
     #msg_bin2=(" ".join(f"{ord(i):08b}" for i in msg)).split(sep=" ")
     msg_bin=[]
     for i in msg:
         msg_bin.append(decimal_bin_conversion(ord(i)))
-    print(msg_bin)
+    return msg_bin
+    #print(msg_bin)
     #print(msg_bin2)
     #bin_msg_conversion(msg_bin)
 
@@ -43,10 +56,10 @@ def bin_msg_conversion(bin_value):
     print("converted : ",msg)
 
 #Converts the modified pixel data into the  final Image
-def pixel_img_conversion(pixel,im):
+def pixel_img_conversion(img_name,pixel,im):
     img_out=Image.new(im.mode,im.size) #creating a new image object
     img_out.putdata(pixel)
-    img_out.save('test_out.jpg')
+    img_out.save(img_name)
 
 #Extracts the pixel values from the given image file
 def img_pixel_extraction(file_name):
@@ -54,5 +67,26 @@ def img_pixel_extraction(file_name):
     im=Image.open(file_name)
     pix_val = list(im.getdata())
     #print(pix_val)
-    pixel_img_conversion(pix_val,im)
-img_pixel_extraction('./testing_1.jpg')
+    #pixel_img_conversion(pix_val,im)
+    return pix_val,im
+#img_pixel_extraction('./testing_1.jpg')
+
+def initializer():
+    clear()
+    choice=int(input("Enter:\n '1' to Encode the image\n '0' to Decode the image\n '5' to exit"))
+    if choice == 1:
+        msg=str(input("Enter the message you want to encode: \n"))
+        img_name=str(input("Enter the name of the image with the extention: "))
+        img_out_name=str(input("Enter the name of the encoded image with the extention: "))
+        msg_bin_list=msg_bin_conversion(msg)
+        img_pixel_list,im= img_pixel_extraction(img_name)
+        img_pixel_list_out=fun_name(msg_bin_list,img_pixel_list)
+        pixel_img_conversion(img_out_name, img_pixel_list_out, im)
+    elif choice == 0:
+        """decoder code"""
+    elif choice == 5:
+        exit()
+    else:
+        print("Invalid entry pls try again is 3sec")
+        time.sleep(3)
+        initializer()
