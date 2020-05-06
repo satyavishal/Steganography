e344d9f921432a81a4fc4391307a408d31f9e53d
Satya Vishal Thota <skillzworks.18@gmail.com>
2020-05-06 22:15:47 +0530

    Defined a function to convert binary value to decimal number which is the acsii value of the character

--- image_steganography.py
+++ image_steganography.py
@@ -1,8 +1,22 @@
-# function converts the message to binary value
+# function converts the message to binary value list
 def msg_bin_conversion():
     msg=str(input("Enter your message here: \n"))
     #converts each character to the bin of its acsii value and joins it to the string.
     # The string is then split into a list
     msg_bin=(" ".join(f"{ord(i):08b}" for i in msg)).split(sep=" ")
     print(msg_bin)
-msg_bin_conversion()
+#msg_bin_conversion()
+
+#def bin_msg_conversion(bin_value):
+#    msg=""
+#    for i in bin_value:
+#        msg=msg.append()
+
+def bin_decimal_conversion(x):
+    c=7
+    decimal=0
+    for i in x:
+        decimal=decimal+(int(i)*(2**c))
+        c-=1
+    print(decimal)
+bin_decimal_conversion("01001000")
